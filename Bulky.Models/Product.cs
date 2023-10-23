f122d3f41c697051d92cb47022e87cb8c7ade07e
Adnan Howlader <adnanhowlader@Adnans-MacBook-Pro.local>
2023-10-23 16:10:46 +0600

    added product

--- Bulky.Models/Product.cs
+++ Bulky.Models/Product.cs
@@ -1,6 +1,45 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace Bulky.Models;
 
 public class Product
 {
+    [Key]
+    public int Id { get; set; }
+    
+    [Required]
+    public string Title { get; set; }
+    public string Description { get; set; }
+    
+    [Required]
+    public string Director { get; set; }
+    
+    [Required]
+    [Range(1,1000)]
+    public string ListPrice { get; set; }
+    
+    [Required]
+    [Range(1,1000)]
+    [DisplayName("Price 1-50")]//this is the name that will be displayed on the page
+    public double Price { get; set; }
+    
+    [Required]
+    [Range(1,1000)]
+    [DisplayName("Price for 50+")]
+    public double Price50 { get; set; }
+    
+    [Required]
+    [Range(1,1000)]
+    [DisplayName("Price for 100+")]
+    public double Price100 { get; set; }
+    
+    
+    
+    
+    
+   
+    
+    
     
 }
